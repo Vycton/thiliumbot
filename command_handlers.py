40ee23bcd73a3f678c4f187c6ed216e71547e593
Marco Breemhaar <marcobreemhaar@gmail.com>
2020-04-04 23:49:58 +0200

    Added teams feature using voice channel members
    e.g. '$teams 2 "Apen Energy"' divides all members of voice channel "Apen Energy" over 2 equally sized groups. Closes #1

--- command_handlers.py
+++ command_handlers.py
@@ -1,5 +1,6 @@
 import random
 import itertools
+import re
 
 # Any command starting with $ will execute its corresponding function here.
 
@@ -17,17 +18,32 @@ def lotto(message):
 # First argument specifies number of teams. Other arguments are player names.
 # Players are then randomly divided over equally large teams.
 def teams(message):
-    try:
-        num_teams = int(message.content.split()[1])
+    channel_team_re = re.compile('\$teams (?P<n_teams>\d+) \"(?P<channel_name>.+)\"')
+
+    if channel_team_re.match(message.content):
+        num_teams = int(channel_team_re.search(message.content).group('n_teams'))
+        channel_name = channel_team_re.search(message.content).group('channel_name')
+
+        voice_channels = [channel for channel in message.guild.voice_channels if channel.name == channel_name]
+        if voice_channels:
+            options = [member.name for member in voice_channels[0].members if member.bot is False]
+        else:
+            return 'Could not find that channel'
+    else:
+        try:
+            num_teams = int(message.content.split()[1])
+            options = message.content.split()[2:]
+        except (ValueError, IndexError):
+            return "Give me the number of teams you need and the names of the people you are with. I'll make you some teams."
+
         if num_teams < 1:
-            return 'You need at least one team.'
-
-        options = message.content.split()[2:]
-
-        if len(options) == 0:
-            return "I can't make any teams if there are no people."
-    except (ValueError, IndexError):
-        return "Give me the number of teams you need and the names of the people you are with. I'll make you some teams."
+            return "How do you expect me to make {} teams?".format(num_teams)
+        elif len(options) == 0:
+            return "It's a bit difficult making teams without any people, isn't it?"
+        elif len(options) < num_teams:
+            return "More teams than there are people? Well, that's not going to work..."
+    
+    
     random.shuffle(options)
 
     teams = [[] for _ in range(num_teams)]
