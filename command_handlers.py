f9903e1464f092ee5d115cd58053dd67f6318ed0
Marco Breemhaar <marcobreemhaar@gmail.com>
2020-04-04 21:28:02 +0200

    Handle some incorrect input in lotto and teams

--- command_handlers.py
+++ command_handlers.py
@@ -9,14 +9,25 @@ def test(message):
 
 # Take any number of arguments and return one of them randomly.
 def lotto(message):
+    if len(message.content.split()) < 2:
+        return "I can't choose anything if you don't give me any options!"
     options = message.content.split()[1:]
     return random.choice(options)
 
 # First argument specifies number of teams. Other arguments are player names.
 # Players are then randomly divided over equally large teams.
 def teams(message):
-    num_teams = int(message.content.split()[1])
-    options = message.content.split()[2:]
+    try:
+        num_teams = int(message.content.split()[1])
+        if num_teams < 1:
+            return 'You need at least one team.'
+
+        options = message.content.split()[2:]
+
+        if len(options) == 0:
+            return "I can't make any teams if there are no people."
+    except (ValueError, IndexError):
+        return "Give me the number of teams you need and the names of the people you are with. I'll make you some teams."
     random.shuffle(options)
 
     teams = [[] for _ in range(num_teams)]
